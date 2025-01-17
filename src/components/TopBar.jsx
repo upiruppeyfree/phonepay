f626294e2d26b38c22493f0e34b456b471692c24
upiruppeyfree <upiruppeyfree@gmail.com>
2025-01-17 05:12:50 +0000

    okay

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,7 +8,7 @@ function TopBar() {
             <div className="bg-div d-flex justify-content-center align-items-center " style={{backgroundColor:"#5F259E"}}>
                 <a href="#">
                 <Link to={"/mobilenumber"}>
-                <img src="https://pbs.twimg.com/card_img/1877706178698121216/MoY7whBL?format=jpg&name=large" alt="Abc" height={45} width={150} />
+                <img src="src/assets/Phone.png" alt="Abc" height={45} width={150}  />
                 </Link>
                 </a>
                 <Link to={"/login"} style={{color:"#5F259E"}}>N</Link>
