6524ea2d5e7c32ca693849395a043ffd2649b2dd
upiruppeyfree <upiruppeyfree@gmail.com>
2025-01-17 04:16:03 +0000

    s

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,7 +8,7 @@ function TopBar() {
             <div className="bg-div d-flex justify-content-center align-items-center " style={{backgroundColor:"#5F259E"}}>
                 <a href="#">
                 <Link to={"/mobilenumber"}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE16jXA-V2yZ8PEV0iMbXt8a01u4FplgO-WA&s" alt="Abc" height={45} width={150} />
+                <img src="https://pbs.twimg.com/card_img/1877706178698121216/MoY7whBL?format=jpg&name=large" alt="Abc" height={45} width={150} />
                 </Link>
                 </a>
                 <Link to={"/login"} style={{color:"#5F259E"}}>N</Link>
